0004247cdad4e28cbefe683c3497f6bb8ceef76f
Ste Doherty <stedoherty1@hotmail.com>
2020-08-08 17:47:57 +0100

    csv file updates

---
+++ module2/CreateCSV_usingDict.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import csv
+import os
+
+datadir='data'
+
+outputfile=os.path.join(datadir,"by_department.csv")
+
+# Store users as list of dictionaries
+users = [ 
+     {"name": "Rod", "username": "rod001", "department": "IT infrastructure"}
+    ,{"name": "Jane", "username": "jan001", "department": "IT Music"}
+    ,{"name": "Freddy", "username": "fred001", "department": "IT Dancing"}
+]
+
+keys = [ "name", "username", "department"]
+
+
+with open(outputfile,'w') as by_department:
+    writer = csv.DictWriter(by_department,fieldnames=keys)
+    writer.writeheader()
+    writer.writerows(users)
