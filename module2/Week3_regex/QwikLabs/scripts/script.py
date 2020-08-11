617c7c8dfab3a4a55ffb06b4fd76ecb81bfb521a
ste doherty <stedoherty1@hotmail.com>
2020-08-11 11:01:00 +0000

    start of Qwiklabs week3

---
+++ module2/Week3_regex/QwikLabs/scripts/script.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+#Import libraries
+
+def contains_domain(address, domain):
+  """Returns True if the email address contains the given domain,
+    in the domain position, false if not."""
+  return False
+
+def replace_domain(address, old_domain, new_domain):
+  """Replaces the old domain with the new domain in
+    the received address."""
+  return address
+
+def main():
+  """Processes the list of emails, replacing any instances of the
+    old domain with the new domain."""
+
+main()
