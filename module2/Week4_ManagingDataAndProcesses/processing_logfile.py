b819a09b2e50ff0fde8fa9be0586e2ea44c956eb
Ste Doherty <stedoherty1@hotmail.com>
2020-08-13 12:58:02 +0100

    update

--- module2/Week4_ManagingDataAndProcesses/processing_logfile.py
+++ module2/Week4_ManagingDataAndProcesses/processing_logfile.py
@@ -7,10 +7,20 @@ import sys
 import argparse
 
 import re
+import os 
 
+this_script_path=(os.path.dirname(sys.argv[0]))
+print('this_script_path:' + this_script_path)
+
+this_script_name=(os.path.basename(sys.argv[0]))
+datadir=os.path.join(this_script_path,"data") # Ah!  you don't want the '/' in your args - makes sense!
+
+print('datadir:' + datadir)
+default_logfile=os.path.join(datadir,"syslog_example")
+print('default is :' + default_logfile)
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--logfile", type=str, dest="logfile", help="the logfile to be processed", default="/etc/hosts")
+parser.add_argument("--logfile", type=str, dest="logfile", help="the logfile to be processed", default=default_logfile)
 
 args = parser.parse_args()
 
@@ -19,15 +29,23 @@ print('logfile is :' + args.logfile )
 
 #logfile = sys.argv[1]
 logfile = args.logfile 
-interesting_thing='ip'
+interesting_thing='CRON'
+usernames={} # create empty dictionary 
 with open(logfile,'r') as f:
     for line in f:
         if interesting_thing not in line:
             continue
-        # Do some pattern matchign , to get some info
-        # and then print it out
-        print(line.strip())
+        # Do some pattern matching , to get some info
+        pattern = r"USER \((\w+)\)$"
+        result = re.search(pattern, line)
 
+        if result is None:
+            continue
+        
+        name = result[1]
+        usernames[name] = usernames.get(name,0) + 1  #tell get to  use default of '0'
+
+print(usernames)
 
 
 # We're using the syslog, and we want to display the date, time, and process id 
@@ -43,8 +61,10 @@ def show_time_of_pid(line):
     result = re.search(pattern, line)
     return "{} {} pid:{}".format(result[1],result[2],result[3])
 
-print(show_time_of_pid("Jul 6 14:01:23 computer.name CRON[29440]: USER (good_user)")) # Jul 6 14:01:23 pid:29440
-print(show_time_of_pid("Jul 6 14:02:08 computer.name jam_tag=psim[29187]: (UUID:006)")) # Jul 6 14:02:08 pid:29187
-print(show_time_of_pid("Jul 6 14:02:09 computer.name jam_tag=psim[29187]: (UUID:007)")) # Jul 6 14:02:09 pid:29187
-print(show_time_of_pid("Jul 6 14:03:01 computer.name CRON[29440]: USER (naughty_user)")) # Jul 6 14:03:01 pid:29440
-print(show_time_of_pid("Jul 6 14:03:40 computer.name cacheclient[29807]: start syncing from \"0xDEADBEEF\"")) # Jul 6 14:03:40 pid:29807
+#print(show_time_of_pid("Jul 6 14:01:23 computer.name CRON[29440]: USER (good_user)")) # Jul 6 14:01:23 pid:29440
+#print(show_time_of_pid("Jul 6 14:02:08 computer.name jam_tag=psim[29187]: (UUID:006)")) # Jul 6 14:02:08 pid:29187
+#print(show_time_of_pid("Jul 6 14:02:09 computer.name jam_tag=psim[29187]: (UUID:007)")) # Jul 6 14:02:09 pid:29187
+#print(show_time_of_pid("Jul 6 14:03:01 computer.name CRON[29440]: USER (naughty_user)")) # Jul 6 14:03:01 pid:29440
+#print(show_time_of_pid("Jul 6 14:03:40 computer.name cacheclient[29807]: start syncing from \"0xDEADBEEF\"")) # Jul 6 14:03:40 pid:29807
+
+
