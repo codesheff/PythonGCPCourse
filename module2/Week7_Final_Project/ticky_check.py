d87ad506cb671d30417d6513973316d41971eb16
stedoherty <stedoherty1@hotmail.com>
2020-08-19 15:08:24 +0000

    updates

--- module2/Week7_Final_Project/ticky_check.py
+++ module2/Week7_Final_Project/ticky_check.py
@@ -6,7 +6,45 @@ import argparse, os, sys
 
 
 
+def search_logfile(log_file):
+    
+    import re
 
+    #Initialise your dictionary
+    error_messages = {}
+    per_user = {}
+    
+    with open(log_file, mode='r',encoding='UTF-8') as file:
+        for log_entry in  file.readlines():
+            #print(log_entry)
+
+            pattern = r'ticky: INFO ([\w ]*) '
+            result = re.search(pattern, log_entry)         
+            if result:
+                print(log_entry)
+                message_type='INFO'
+                user = result[1]
+            
+            pattern = r'ticky: ERROR ([\w ]*) '
+            result = re.search(pattern, log_entry)         
+            if result:
+                print(log_entry)
+                message_type='ERROR'
+                user = result[1]
+
+            print(log_entry)
+            print(user)
+            print(message_type)
+
+            #Create list of error patterns, as the word 'error' , plus whatever words user inputs.
+            #error_patterns = ["error"]
+            #for i in range(len(error.split(' '))):
+            #    error_patterns.append(r"{}".format(error.split(' ')[i].lower()))
+            #    # If all the patterns are matched, the include that in the list of returned errors
+            #    if all(re.search(error_pattern, log.lower()) for error_pattern in error_patterns):
+            #        returned_errors.append(log)
+        file.close()
+    return error_messages, per_user
 
 
 if __name__ == "__main__":
@@ -14,10 +52,10 @@ if __name__ == "__main__":
     print('this_script_path:' + this_script_path)
     
     this_script_name=(os.path.basename(sys.argv[0]))
-    datadir=os.path.join(this_script_path,"../data") # Ah!  you don't want the '/' in your args - makes sense!
+    datadir=os.path.join(this_script_path,"") # Ah!  you don't want the '/' in your args - makes sense!
     
     print('datadir:' + datadir)
-    default_logfile=os.path.join(datadir,"fishy.log")
+    default_logfile=os.path.join(datadir,"syslog.log")
     print('default is :' + default_logfile)
     
     # Set up input options
@@ -28,3 +66,9 @@ if __name__ == "__main__":
     print('logfile is :' + args.logfile )
     #logfile = sys.argv[1]
     log_file = args.logfile 
+
+   
+
+
+    ## Read log file
+    search_logfile(log_file)
