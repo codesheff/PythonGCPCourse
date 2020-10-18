33cc6288cd8bf49eabc3f764bf43862567a81a69
Code Sheff <code_sheff@outlook.com>
2020-10-18 08:48:11 +0100

    Fix code to FixImages.py

--- capstone/week1/scripts/FixImages.py
+++ capstone/week1/scripts/FixImages.py
@@ -11,40 +11,40 @@ import PIL.Image
 
 this_script_path=(os.path.dirname(sys.argv[0]))
 this_script_name=(os.path.basename(sys.argv[0]))
-datadir=os.path.join(this_script_path,"../images")
-outdir='/opt/icons'
+datadir=os.path.abspath(os.path.join(this_script_path,"../images"))
+#outdir='/opt/icons'
+outdir='/f/stedo/mygit/PythonGCPCourse/capstone/week1/icons'
 
 
-def process_file(root,filename):
+def process_file(file):
     
     #from PIL import Image
-    file=os.path.join(root,filename)
+    #file=os.path.join(root,filename)
     print('Processing ' + file)
     
     
     image=PIL.Image.open(file)
 
-   
-    #size =  (128, 128)
-    #image.resize(size)
-    #image.thumbnail(size)
-    #image.rotate(-90)
-
+    size =  (128, 128)
+    image.resize(size)
+    image.rotate(-90)
+    #image.convert('RGB')
+    image.rotate(-90).convert('RGB').resize((128,128))
+    outfile = os.path.join(outdir,os.path.basename(file)).split('.')[0] 
+    image.save(outfile,'jpeg')
     
-    #outfile = os.path.join(outdir,filename)
-    outfile = os.path.join(outdir,'stetest.jpg')
-    image.save(outfile, "JPEG")
-    #return new_name
-
+    print('saving as ' + outfile)
+  
+    image.close()
 
 def main():
-    for root, dirs, files in os.walk(datadir):
-        for filename in files:
-            if filename == '.DS_Store':
-                continue
+    for filename in os.listdir(datadir):
+        if filename.startswith('.'):       # skip files that start with '.'
+            continue
             
-            #print(root + ' ' + filename )
-            process_file(root,filename)
-            break
+        #print(root + ' ' + filename )
+        file=os.path.join(datadir,filename)
+        process_file(file)
+        break
 
 main()
